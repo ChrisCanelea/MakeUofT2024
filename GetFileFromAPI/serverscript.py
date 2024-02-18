4f97b1ae121fb2691d3fd8d76485fcbf2082c1e7
Chris <chris.canelea@mail.utoronto.ca>
2024-02-18 02:17:54 -0500

    functional

--- GetFileFromAPI/serverscript.py
+++ GetFileFromAPI/serverscript.py
@@ -20,24 +20,24 @@ while True:
 
     data = data.decode('utf-8') # decode from serial (decode ASCII)
         
-    if data[1] == "+": # route number query in form "/+<number>\r\n"
-        route = int(data[1:-2]) # discard first char, last 2 char, and split on ":"
-        print(str(route)) # debug print query
+    if data[1] == "&": # route number query in form "/&<number>\r\n"
+        route = data[2:-2] # discard first char, last 2 char
+        print("Fetching stops on route: " + route) # debug print query
 
         # pass route number to data script
         # returns list of bus stations (stops) for a given route
-        station_list = subprocess.check_output(["python","getSchedule.py",route])
+        station_list = subprocess.check_output(["python","getStations.py",route])
 
         print(station_list.decode('utf-8')) # debug print result of route number query
         server.write(station_list) # send station list result back to server
     
-    elif data[1] == "-": # station name query in form "/-<station name>\r\n"
-        station = data[1:-2] # discard first char, last 2 char
-        print(station) # debug print query
+    elif data[1] == "$": # station name query in form "/$<station name>\r\n"
+        station = data[2:-2] # discard first char, last 2 char
+        print("Fetching bus times for: " + station) # debug print query
 
         # pass station name to data script
         # returns bus times for previously queried route #
-        bus_times = subprocess.check_output(["python","getStations.py",station])
+        bus_times = subprocess.check_output(["python","getSchedule.py",station])
 
         print(bus_times.decode('utf-8')) # debug print result of station name query
         server.write(bus_times) # send time result back to server
