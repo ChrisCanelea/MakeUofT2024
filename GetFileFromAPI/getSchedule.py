e8482b6301509a67bca3b29c8d8d7cee0385eed6
Jzhaoqc <zhaoqianchi20021008@gmail.com>
2024-02-18 01:57:09 -0500

    splitting getSchedule into two functions

--- GetFileFromAPI/getSchedule.py
+++ GetFileFromAPI/getSchedule.py
@@ -2,7 +2,6 @@ import xml.dom.minidom
 import sys
 import requests
 import os
-import re
 
 # https://retro.umoiq.com/xmlFeedDocs/NextBusXMLFeed.pdf
 
@@ -17,71 +16,6 @@ def outSchedule():
         # min = prediction.getAttribute('minutes')
         sec = prediction.getAttribute('seconds')
         print(f"{sec}")
-
-
-def getBusStops(busID):
-    # Send an HTTP GET request to the URL
-    body = 'https://retro.umoiq.com/service/publicXMLFeed?command=routeConfig&a=ttc&r='
-    url = body+busID
-    response = requests.get(url)
-
-    folder_path = './xml_files'
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-
-        xml_content = response.content
-        # Create the folder if it doesn't exist
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-
-        # Save the XML content to a file in the specified folder
-        with open(os.path.join(folder_path, 'busStopLUT.xml'), 'wb') as f:
-            f.write(xml_content)
-            
-        #print("XML file downloaded successfully.")
-    else:
-        # If the request was not successful, print the status code
-        print(f"Failed to download XML file. Status code: {response.status_code}")
-
-
-#this function takes the busID, station name and check if they are valid
-def checkBusId(busID):
-    idIsValid = False
-
-    #we check if the bus id exist in the LUT
-    domtree = xml.dom.minidom.parse('./xml_files/ttcLUT.xml')
-    group = domtree.documentElement
-    routes = group.getElementsByTagName('route')
-    #check if busID is in the LUT
-    for route in routes:
-        tag = route.getAttribute('tag')
-        if busID == tag:
-            idIsValid = True
-            break
-
-    # if the bus id is valid, get bus stop from API 
-    if idIsValid:
-        getBusStops(busID)
-    #if not, return error
-    else:
-        print("Bus ID is not valid")
-        sys.exit(1)
-
-def getStationId(stationName):
-    #we check if the station exist in the LUT
-    domtree = xml.dom.minidom.parse('./xml_files/busStopLUT.xml')
-    group = domtree.documentElement
-    stops = group.getElementsByTagName('stop')
-    #check if busID is in the LUT
-    for stop in stops:
-        title = stop.getAttribute('title')
-        tag = stop.getAttribute('tag')
-        if stationName == title and tag[-3:]!="_ar":
-            return stop.getAttribute('stopId')
-    
-    print("No station match")
-    sys.exit(1)
 
 def getPrediction(stopID):
     # Send an HTTP GET request to the URL
@@ -108,17 +42,30 @@ def getPrediction(stopID):
     else:
         # If the request was not successful, print the status code
         print(f"Failed to download XML file. Status code: {response.status_code}")
- 
+
+def getStationId(stationName):
+    #we check if the station exist in the LUT
+    domtree = xml.dom.minidom.parse('./xml_files/busStopLUT.xml')
+    group = domtree.documentElement
+    stops = group.getElementsByTagName('stop')
+    #check if busID is in the LUT
+    for stop in stops:
+        title = stop.getAttribute('title')
+        tag = stop.getAttribute('tag')
+        if stationName == title and tag[-3:]!="_ar":
+            return stop.getAttribute('stopId')
+    
+    print("No station match")
+    sys.exit(1)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python3 getSchedule.py <bus #> <station name>")
+    if len(sys.argv) != 2:
+        print("Usage: python3 getSchedule.py <station name>")
         sys.exit(1)
     
-    busID = sys.argv[1]
-    stationName = sys.argv[2]
+    stationName = sys.argv[1]
 
-    checkBusId(busID)
     stopId = getStationId(stationName)
     getPrediction(stopId)
     outSchedule()
