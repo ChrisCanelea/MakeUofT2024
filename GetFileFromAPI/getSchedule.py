a3346a581034d410f3f21f292197ba2b63fad5f4
Jzhaoqc <zhaoqianchi20021008@gmail.com>
2024-02-17 22:22:08 -0500

    merged getSchedule into one func

--- GetFileFromAPI/getSchedule.py
+++ GetFileFromAPI/getSchedule.py
@@ -69,7 +69,6 @@ def checkBusId(busID):
         sys.exit(1)
 
 def getStationId(stationName):
-
     #we check if the station exist in the LUT
     domtree = xml.dom.minidom.parse('./xml_files/busStopLUT.xml')
     group = domtree.documentElement
@@ -82,7 +81,33 @@ def getStationId(stationName):
     
     print("No station match")
     sys.exit(1)
-    
+
+def getPrediction(stopID):
+    # Send an HTTP GET request to the URL
+    body = 'https://retro.umoiq.com/service/publicXMLFeed?command=predictions&a=ttc&stopId='
+    url = body+stopID
+    response = requests.get(url)
+
+    folder_path = './xml_files'
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+
+        xml_content = response.content
+
+        # Create the folder if it doesn't exist
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        # Save the XML content to a file in the specified folder
+        with open(os.path.join(folder_path, 'stopPrediction.xml'), 'wb') as f:
+            f.write(xml_content)
+            
+        print("XML file downloaded successfully.")
+    else:
+        # If the request was not successful, print the status code
+        print(f"Failed to download XML file. Status code: {response.status_code}")
+ 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -94,5 +119,5 @@ if __name__ == "__main__":
 
     checkBusId(busID)
     stopId = getStationId(stationName)
-    subprocess.call(["python3", "./getXML.py", stopId])
+    getPrediction(stopId)
     outSchedule()
